1e838c09ddf630a47efa3bcdb67b86ae96b9630e
Petja Pajunen <petja.pajunen@elisanet.fi>
2024-12-13 17:49:25 +0200

    commit

--- Module_3/module3-starters-main/t1/1.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const targetElement = document.getElementById("target");
-    targetElement.innerHTML = `
-        <li>First item</li>
-        <li>Second item</li>
-        <li>Third item</li>
-    `;
-});
